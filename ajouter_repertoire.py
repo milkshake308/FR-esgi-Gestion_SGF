b4ea15331b1455414171124c0799d830ba26c83a
Nessim <jaballah.nessim@gmail.com>
2023-05-25 19:58:24 +0200

    add modif

---
+++ ajouter_repertoire.py
@@ -0,0 +1,4 @@
+import modification
+
+chemin_parent = 'C:/Users/njaba/Documents/Python S6'
+modification.ajouter_repertoire(chemin_parent, nouveau_nom, repertoire=Fals)
---
+++ renommer.py
@@ -0,0 +1,9 @@
+import modification
+
+
+
+
+chemin_repertoire = 'C:/Users/njaba/Documents/Python S5'
+nouveau_nom = 'C:/Users/njaba/Documents/Python S10'
+
+modification.renommer_repertoire(chemin_repertoire,nouveau_nom)
