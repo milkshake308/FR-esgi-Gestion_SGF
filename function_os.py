b0fab18f5c06c3957ff400d4e5c2864796427fdf
milkshake308 <milkshake.2734@gmail.com>
2023-05-27 15:17:28 +0200

    changement pour que le code traite le repertoire courant et suppresion de code redondant

--- function_os.py
+++ function_os.py
@@ -4,31 +4,22 @@ from prettytable import PrettyTable
 
 def lister_contenu_dossier(chemin):
     tableau = PrettyTable()
-    tableau.field_names = ["Type", "Permission", "Owner", "GROUPE", "TAILLE", "Datetime modif", "Nom", "RÃ©pertoire Parents"]
+    tableau.field_names = ["TYPE", "MASK", "OWNER", "GROUP", "SIZE (B)", "LAST EDIT", "FILENAME", "PARENT DIRECTORY"]
 
-    for dossier_racine, sous_dossiers, fichiers in os.walk(chemin):
-        for nom in fichiers:
-            chemin_absolu = os.path.join(dossier_racine, nom)
-            infos_fichier = os.stat(chemin_absolu)
-            permissions = oct(infos_fichier.st_mode)[-3:]
-            owner = infos_fichier.st_uid
-            groupe = infos_fichier.st_gid
-            taille = infos_fichier.st_size
-            date_modif = datetime.fromtimestamp(infos_fichier.st_mtime)
-            nom_parent = os.path.basename(dossier_racine)
-
-            tableau.add_row(["Fichier", permissions, owner, groupe, taille, date_modif, nom, nom_parent])
-
-        for nom in sous_dossiers:
-            chemin_absolu = os.path.join(dossier_racine, nom)
-            infos_dossier = os.stat(chemin_absolu)
-            permissions = oct(infos_dossier.st_mode)[-3:]
-            owner = infos_dossier.st_uid
-            groupe = infos_dossier.st_gid
-            taille = infos_dossier.st_size
-            date_modif = datetime.fromtimestamp(infos_dossier.st_mtime)
-            nom_parent = os.path.basename(dossier_racine)
-
-            tableau.add_row(["Dossier", permissions, owner, groupe, taille, date_modif, nom, nom_parent])
+    for nom in os.listdir(chemin):
+        chemin_absolu = os.path.join(chemin, nom)
+        infos_fichier = os.stat(chemin_absolu)
+        permissions = oct(infos_fichier.st_mode)[-3:]
+        owner = infos_fichier.st_uid
+        groupe = infos_fichier.st_gid
+        taille = infos_fichier.st_size
+        date_modif = datetime.fromtimestamp(infos_fichier.st_mtime)
+        nom_parent = os.path.basename(chemin)
+        if os.path.isfile(chemin_absolu):
+            tableau.add_row(["FILE", permissions, owner, groupe, taille, date_modif, nom, nom_parent])
+        elif os.path.isdir(chemin_absolu):
+            tableau.add_row(["DIR", permissions, owner, groupe, taille, date_modif, nom, nom_parent])
+        else:
+            tableau.add_row(["SPECIAL", permissions, owner, groupe, taille, date_modif, nom, nom_parent])
 
     return tableau
